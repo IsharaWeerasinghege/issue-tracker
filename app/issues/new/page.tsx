4dd32918d8dfd4d72e3c409cfdd08d9874df8e87
ishara <surangaishara8@gmail.com>
2023-11-12 09:22:53 +0530

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,20 +2,47 @@
 import {Button, TextField} from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import {useForm, Controller} from "react-hook-form";
+import axios from "axios";
+import {useRouter} from "next/navigation";
+import toast from "react-hot-toast";
+
+interface IssueForm {
+    title: string;
+    description: string;
+}
 
 function NewIssues() {
+    const { register, handleSubmit, control } = useForm<IssueForm>();
+    const router = useRouter();
+
     return (
-        <div className={'max-w-xl space-y-3 mx-auto text-center'}>
+        <form
+            className={'max-w-xl space-y-3 mx-auto'}
+            onSubmit={handleSubmit( async (data) => {
+                await axios.post('/api/issues', data)
+                    .then(() => {
+                        toast.success('Issue created successfully');
+
+                        setTimeout(() => {
+                            router.push('/issues');
+                        }, 1000);
+                    }).catch((err) => {
+                        toast.error('Failed to create issue');
+                        console.log(err);
+                    });
+            })}
+        >
             <TextField.Root>
-                <TextField.Input placeholder={'Title'}/>
+                <TextField.Input placeholder={'Title'} {...register('title')} />
             </TextField.Root>
 
-            <SimpleMDE placeholder={'Description'}  />
+            <Controller name={'description'} control={control} render={({field}) => <SimpleMDE placeholder={'Description'} {...field} />} />
 
             <Button>
                 Create new issue
             </Button>
-        </div>
+        </form>
     );
 }
 
