a8aedf8c6ca885a13881506bfdd3f3a017455d1d
ishara <surangaishara8@gmail.com>
2023-11-13 10:23:06 +0530

    style the issue details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import prisma from "@/prisma/client";
 import {notFound} from "next/navigation";
+import {Card, Flex, Heading, Text} from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 
 interface Props {
     params: {
@@ -23,10 +25,14 @@ const IssueDetails = async ({params}: Props) => {
 
     return (
         <div>
-            <p>{issue?.title}</p>
-            <p>{issue?.description}</p>
-            <p>{issue?.status}</p>
-            <p>{issue?.createdAt.toDateString()}</p>
+            <Heading className={'capitalize'}>{issue?.title}</Heading>
+            <Flex className={'my-4 gap-2'}>
+                <IssueStatusBadge status={issue?.status}/>
+                <Text>{issue?.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card>
+                <p>{issue?.description}</p>
+            </Card>
         </div>
     );
 }
